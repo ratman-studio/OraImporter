5e880c1daf068c84a6c474eda73c7804b11ae7fa
Hubert Niećko <hubert.niecko@gmail.com>
2020-10-04 13:06:39 +0200

    New game slot Liberty Bell simulator

---
+++ Assets/ScriptableObjects/Variables/FloatVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatVariable : ScriptableObject
+{
+    public float InitialValue;
+    [NonSerialized]
+    public float RuntimeValue;
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitialValue;
+    }
+
+    public void OnBeforeSerialize() {}
+}
