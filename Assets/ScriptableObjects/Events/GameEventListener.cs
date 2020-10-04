612e41a93a623598deb342d65330db5fac7e6cda
Hubert Niećko <hubert.niecko@gmail.com>
2020-10-04 13:38:27 +0200

    Base Event sysstem as in https://unity.com/how-to/architect-game-code-scriptable-objects

---
+++ Assets/ScriptableObjects/Events/GameEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace com.szczuro.events
+{
+    public class GameEventListener:MonoBehaviour
+    {
+        public GameEvent Event;
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+        internal void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+    }
+}
