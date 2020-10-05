6fde40d2219c288eeb027ed2cfe2f3ae29f721ec
Hubert Niećko <hubert.niecko@gmail.com>
2020-10-05 21:14:26 +0200

    Add slot data and prepare unittests

---
+++ Assets/Data/InfoData.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+namespace com.szczuro.slots.data
+{
+    [CreateAssetMenu(fileName = "InfoDat", menuName = "Slots SO/Slot Info", order = 1)]
+    public class InfoData:ScriptableObject
+    {
+        public int WaysToWin;
+        public int TotalWaysToWin;
+        public int TotalPayout;
+        public int TotalWays;
+        public float HitFrequency;
+        public float ReturnToPlayer;
+        public float AverageSpinsUntilWin;
+    }
+}
---
+++ Assets/Data/SlotData.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+
+using UnityEngine;
+namespace com.szczuro.slots.data
+{
+    [CreateAssetMenu(fileName = "SlotData", menuName = "Slots/SO/Slots/SlotData", order = 1)]
+    public class SlotData : ScriptableObject
+    {
+        public string Name;
+        public int MinBet;
+        public int MaxBet;
+        public int Paylines;
+
+        public Dictionary<int, string> StopTypes;
+        public List<Dictionary<int, int>> Reels;
+
+    }
+}
