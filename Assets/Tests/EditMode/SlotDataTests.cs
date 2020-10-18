81c02dded03c258b2c4b0d3738151695473f44d8
Hubert Niećko <hubert.niecko@gmail.com>
2020-10-18 15:29:19 +0200

    divade stodData Test to two files

--- Assets/Tests/EditMode/SlotDataTests.cs
+++ Assets/Tests/EditMode/SlotDataTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 using NUnit.Framework;
 
@@ -107,7 +106,7 @@ namespace com.szczuro.slots.data.tests
             Assert.AreEqual(l1, slot.Reels[0].colors);
             Assert.AreEqual(l2, slot.Reels[1].colors);
             Assert.AreEqual(l3, slot.Reels[2].colors);
-            
+   
         }
 
         [Test]
@@ -143,94 +142,6 @@ namespace com.szczuro.slots.data.tests
             Assert.AreEqual(null, slotEmpty.Reels, "Reels not found");
             Assert.AreEqual(null, slotEmpty.Payouts, "Payline not found");
         }
-    }
-    public class SlotData_Instances_Valid_Configuration: SlotTests
-    {
-        private SlotData[] sds;
-        [SetUp]
-        public void BeforeTest()
-        {
-            sds = TestHelper.GetAllInstances<SlotData>();
-        }
-
-        [Test]
-        public void MinBet_LessThan_MaxBet()
-        {
-            foreach (SlotData slot in sds)
-                Assert.LessOrEqual(slot.MinBet, slot.MaxBet);
-        }
-        [Test]
-        public void MinBet_BiggerThan0()
-        {
-            foreach (SlotData slot in sds)
-                Assert.GreaterOrEqual(slot.MinBet, 1);
-        }
-
-        [Test]
-        public void StopType_TwoOrMore()
-        {
-            foreach (SlotData slot in sds)
-                Assert.GreaterOrEqual(slot.StopTypes.Count, 2);
-        }
-
-        [Test]
-        public void PayLines_OneOrMore()
-        {
-            foreach (SlotData slot in sds)
-                Assert.GreaterOrEqual(slot.StopTypes.Count, 1);
-        }
-        [Test]
-        public void Reels_Exists_And_Above0()
-        {
-            foreach (SlotData slot in sds)
-            {
-                Assert.IsNotNull(slot.Reels);
-                Assert.GreaterOrEqual(slot.Reels.Count(), 1);
-            }
-        }
-
-        private bool SlotHasColor(SlotData slot, int color)
-        {
-            foreach (ReelWheel reel in slot.Reels)
-                if (reel.colors.Contains(color))
-                    return true;
-            return false;
-        }
-
-        [Test]
-        public void Reels_Have_All_StopTypes()
-        {
-            foreach (SlotData slot in sds)
-                foreach (string color in slot.StopTypes)
-                    Assert.IsTrue(SlotHasColor(slot, slot.StopTypes.IndexOf(color)));
-        }
-
-        [Test]
-        public void Reels_Have_Only_Colors_From_StopTypes()
-        {
-            foreach (SlotData slot in sds)
-                foreach (ReelWheel reel in slot.Reels)
-                    foreach (int color in reel.colors)
-                    {
-                        Assert.GreaterOrEqual(color,0);
-                        Assert.Less(color, slot.StopTypes.Count());
-                    }
-        }
-        [Test]
-        public void Reels_StopCount_MoreThan1()
-        {
-            foreach (SlotData slot in sds)
-            {
-                if (slot.Reels?.Count() == 0) Assert.Fail();
-
-                foreach (ReelWheel reel in slot.Reels)
-                {
-                    Assert.IsNotNull(reel);
-                    Assert.Greater(reel.colors.Count(), 1);
-                }
-            }
-        }
-
     }
 
     class TestHelper
---
+++ Assets/Tests/EditMode/SlotData_Instances_Valid_Configuration.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+
+namespace com.szczuro.slots.data.tests
+{
+    public class SlotData_Instances_Valid_Configuration: SlotTests
+    {
+        private SlotData[] sds;
+        [SetUp]
+        public void BeforeTest()
+        {
+            sds = TestHelper.GetAllInstances<SlotData>();
+        }
+
+        [Test]
+        public void MinBet_LessThan_MaxBet()
+        {
+            foreach (SlotData slot in sds)
+                Assert.LessOrEqual(slot.MinBet, slot.MaxBet);
+        }
+        [Test]
+        public void MinBet_BiggerThan0()
+        {
+            foreach (SlotData slot in sds)
+                Assert.GreaterOrEqual(slot.MinBet, 1);
+        }
+
+        [Test]
+        public void StopType_TwoOrMore()
+        {
+            foreach (SlotData slot in sds)
+                Assert.GreaterOrEqual(slot.StopTypes.Count, 2);
+        }
+
+        [Test]
+        public void PayLines_OneOrMore()
+        {
+            foreach (SlotData slot in sds)
+                Assert.GreaterOrEqual(slot.StopTypes.Count, 1);
+        }
+        [Test]
+        public void Reels_Exists_And_Above0()
+        {
+            foreach (SlotData slot in sds)
+            {
+                Assert.IsNotNull(slot.Reels);
+                Assert.GreaterOrEqual(slot.Reels.Count(), 1);
+            }
+        }
+
+        private bool SlotHasColor(SlotData slot, int color)
+        {
+            foreach (ReelWheel reel in slot.Reels)
+                if (reel.colors.Contains(color))
+                    return true;
+            return false;
+        }
+
+        [Test]
+        public void Reels_Have_All_StopTypes()
+        {
+            foreach (SlotData slot in sds)
+                foreach (string color in slot.StopTypes)
+                    Assert.IsTrue(SlotHasColor(slot, slot.StopTypes.IndexOf(color)));
+        }
+
+        [Test]
+        public void Reels_Have_Only_Colors_From_StopTypes()
+        {
+            foreach (SlotData slot in sds)
+                foreach (ReelWheel reel in slot.Reels)
+                    foreach (int color in reel.colors)
+                    {
+                        Assert.GreaterOrEqual(color,0);
+                        Assert.Less(color, slot.StopTypes.Count());
+                    }
+        }
+        [Test]
+        public void Reels_StopCount_MoreThan1()
+        {
+            foreach (SlotData slot in sds)
+            {
+                if (slot.Reels?.Count() == 0) Assert.Fail();
+
+                foreach (ReelWheel reel in slot.Reels)
+                {
+                    Assert.IsNotNull(reel);
+                    Assert.Greater(reel.colors.Count(), 1);
+                }
+            }
+        }
+
+    }
+}
