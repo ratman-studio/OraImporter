deb082f1653b9daa48beeeae9f399f1f1d59bab6
Hubert Niećko <hubert.niecko@gmail.com>
2021-01-27 07:00:43 +0100

    start adding custom ediotor to ora

--- Assets/Editor/importers/OraImporter.cs
+++ Assets/Editor/importers/OraImporter.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 using UnityEditor.Experimental.AssetImporters;
+using UnityEditor.Experimental.TerrainAPI;
 using UnityEngine;
+using static com.szczuro.importer.ora.OraImporter;
 
 namespace com.szczuro.importer.ora
 {
     [ScriptedImporter(1, "ora")]
     public class OraImporter : ScriptedImporter
     {
-        enum ImportType
+        public enum ImportType
         {
             Single,
             Multi
         }
 
-        [SerializeField] private ImportType ImportAs = ImportType.Single;
+        [SerializeField] public ImportType ImportAs = ImportType.Single;
         private OraFile _oraFile;
         
 
@@ -73,4 +76,68 @@ namespace com.szczuro.importer.ora
 
         #endregion
     }
+
+    [CustomEditor(typeof(OraImporter))]
+    public class OraImporterEditor : ScriptedImporterEditor
+    {
+        private SerializedProperty ImportAs;
+
+        public override void OnEnable()
+        {
+            ImportAs = serializedObject.FindProperty("ImportAs");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            base.OnInspectorGUI();
+
+            EditorGUI.showMixedValue = ImportAs.hasMultipleDifferentValues;
+            ImportAs.intValue = EditorGUILayout.IntPopup(GUITexts.ImportTypeTitle,  ImportAs.intValue, 
+                GUITexts.ImportTypeOptions, GUITexts.ImportTypeValues);
+            EditorGUI.showMixedValue = false;
+
+            switch ((ImportType)ImportAs.intValue)
+            {
+                case ImportType.Multi :
+                    EditorGUILayout.LabelField("Layers");
+                    MultiLayerImportGUI();
+                    break;;
+                case ImportType.Single:
+                default:
+                    EditorGUILayout.LabelField("Single");
+                    SingleImageImportGUI();
+                    //ImportAs.intValue = (int) ImportType.Single;
+                    break;
+                    
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void SingleImageImportGUI()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        private void MultiLayerImportGUI()
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+
+    internal static class GUITexts
+    {
+        public static GUIContent ImportTypeTitle = new GUIContent("Texture Type", "What will this texture be used for?");
+        public static GUIContent[] ImportTypeOptions =
+        {
+            new GUIContent("Merged", "This is the default Texture Type usage when an image is imported without a specific Texture Type selected"),
+            new GUIContent("Layers", "Texture is imported as layers."),
+        };
+        public static int[] ImportTypeValues =
+        {
+            (int) ImportType.Single,
+            (int) ImportType.Multi
+        };
+    }
+
 }
