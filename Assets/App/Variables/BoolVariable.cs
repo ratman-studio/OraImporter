54e52020a88768f0a3a0e16d484e5626d4bec33b
Hubert Niećko <hubert.niecko@gmail.com>
2021-04-08 22:46:59 +0200

    remove game items

--- Assets/App/Variables/BoolVariable.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using UnityEngine;
-
-namespace com.szczuro.variables
-{
-    [CreateAssetMenu(fileName = "UnnamedBoolVariable", menuName = "Variables/Bool")]
-    public class BoolVariable: Variable<int> { }
-}
--- Assets/App/Variables/IntegerVariable.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using UnityEngine;
-
-namespace com.szczuro.variables
-{
-    [CreateAssetMenu(fileName = "UnnamedIntegerVariable", menuName = "Variables/Integer")]
-    public class IntegerVariable : Variable<int>    {    }
-}
