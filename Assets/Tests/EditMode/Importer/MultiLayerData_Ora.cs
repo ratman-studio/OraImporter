4a2c19a33cdd1a7902266bc4044f4ce50f6673c1
Hubert Niećko <hubert.niecko@gmail.com>
2021-12-08 23:09:19 +0100

    rewrite test

---
+++ Assets/Tests/EditMode/Importer/MultiLayerData_Ora.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace com.szczuro.importer.Tests
+{
+    [TestFixture]
+    public class MultiLayerData_Ora
+    {
+        // setup 
+        private const string FILENAME = "Assets/Tests/EditMode/Importer/files/ora/LibertyBell.ora";
+        private const int EXPECTED_LAYERS = 5;
+        private const string EXPECTED_FILENAME = "LibertyBell";
+        private const string EXPECTED_TEXTURE_NAME = "horsShoe";
+
+        [Test]
+        public void CreatedFile_exits()
+        {
+            var multiLayerData = MultiLayerFileFactory.CreteFileFromPath(FILENAME);
+            Assert.NotNull(multiLayerData);
+        }
+
+        [Test]
+        public void GetMergedLayers_ret_Texture2d()
+        {
+            // Arrange
+            var multiLayerData = MultiLayerFileFactory.CreteFileFromPath(FILENAME);
+            // Act 
+            var mergedLayers = multiLayerData.GetMergedLayers();
+            // Assert
+            Assert.IsTrue(mergedLayers is Texture2D);
+        }
+
+        [Test]
+        public void GetLayers_ret_Layers()
+        {
+            var multiLayerData = MultiLayerFileFactory.CreteFileFromPath(FILENAME);
+            var layers = multiLayerData.GetLayers();
+            Assert.NotNull(layers);
+            Assert.AreEqual(EXPECTED_LAYERS, layers.Count);
+        }
+
+        [Test]
+        public void GetLayers_ret_ListOfTexture2d()
+        {
+            var multiLayerData = MultiLayerFileFactory.CreteFileFromPath(FILENAME);
+            var layers = multiLayerData.GetLayers();
+
+            Assert.Greater(layers.Count, 0);
+            foreach (var layer in layers)
+                Assert.NotNull(layer is Texture2D);
+        }
+
+        [Test]
+        public void GetTextureName_ret_ProperTextureName()
+        {
+            var multiLayerData = MultiLayerFileFactory.CreteFileFromPath(FILENAME);
+            var layers = multiLayerData.GetLayers();
+            var textureName = multiLayerData.GetTextureName(layers[0]);
+            Assert.AreEqual(EXPECTED_TEXTURE_NAME,textureName);
+        }
+
+        [Test]
+        public void GetFileName_ret_FileName()
+        {
+            var multiLayerData = MultiLayerFileFactory.CreteFileFromPath(FILENAME);
+            var filename = multiLayerData.GetFileName();
+            Assert.AreEqual(EXPECTED_FILENAME, filename);
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/Importer/StackParsingTest.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using NUnit.Framework;
+
+namespace com.szczuro.importer.Tests
+{
+    public class StackParsingTest
+    {
+        [Test]
+        public void simple_stackXml_parsing_test()
+        {
+            //ARRANGE
+            var serializer = new XmlSerializer(typeof(OraXMLMain));
+            OraXMLMain i;
+            var filename = "Assets/Tests/EditMode/Importer/files/ora/stack.xml";
+            
+            //ACT
+            using (Stream reader = new FileStream(filename, FileMode.Open))
+            {
+                // Call the Deserialize method to restore the object's state.
+                i = (OraXMLMain) serializer.Deserialize(reader);
+            }
+
+            //ASSERT
+            Assert.IsNotNull(i);
+            Assert.AreEqual(128,i.Width);
+            Assert.AreEqual(128,i.Height);
+            Assert.AreEqual(100,i.XRes);
+            Assert.AreEqual(100,i.YRes);
+            Assert.AreEqual("0.0.1",i.Version);
+  
+        }
+        
+        [Test]
+        public void not_valid_compositeOp_return_valid_Blending()
+        {
+            //ARRANGE
+            var notValidCompositeOp = "svg:not-valid";
+
+            //ACT
+            var blending = Ora.GETBlendingFromCompositeOp(notValidCompositeOp);
+            //ASSERT
+            Assert.AreEqual(blending, Ora.Blending.Normal);
+        }
+
+        [Test]
+        public void not_valid_compositeOp_return_valid_Compositing()
+        {
+            //ARRANGE
+            var notValidCompositeOp = "svg:not-valid";
+            //ACT
+            var compositing = Ora.GETCompositingOperatorFromCompositeOp(notValidCompositeOp);
+            //ASSERT
+            Assert.AreEqual(compositing, Ora.Compositing.SourceOver);
+        }
+    }
+}
